3b487ca5c0adb1bd380e9350d9637da662dccb02
flaviosoliver <flavsoliver@gmail.com>
2023-03-21 12:17:00 -0300

    Updates

---
+++ precache-manifest.914af9fc53034b11df7898f83e141d38.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f16268f47e7c55c9384486b64038919",
+    "url": "/project-frontend-online-store/index.html"
+  },
+  {
+    "revision": "1b3aeda2e8827e1e811e",
+    "url": "/project-frontend-online-store/static/css/main.0662afa8.chunk.css"
+  },
+  {
+    "revision": "a6d210fd9699a7f63221",
+    "url": "/project-frontend-online-store/static/js/2.274461be.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/project-frontend-online-store/static/js/2.274461be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b3aeda2e8827e1e811e",
+    "url": "/project-frontend-online-store/static/js/main.04329580.chunk.js"
+  },
+  {
+    "revision": "cb50648c2a3b9648c18e",
+    "url": "/project-frontend-online-store/static/js/runtime-main.95a35047.js"
+  },
+  {
+    "revision": "0d01d26885c00a5d70669df1e61e439c",
+    "url": "/project-frontend-online-store/static/media/mercadolivre.0d01d268.png"
+  },
+  {
+    "revision": "f24705c033888cc44822f00dbf65d570",
+    "url": "/project-frontend-online-store/static/media/quindimshop.f24705c0.png"
+  }
+]);
